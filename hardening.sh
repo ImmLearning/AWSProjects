abe715c9ff729c978587eee4763c974d888e1838
ImmLearning <51955196+ImmLearning@users.noreply.github.com>
2019-09-26 13:12:27 +0530

    CENTOS 7 hardening

---
+++ hardening.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#"CentOS 7 with below packages"
+yum install -y wget elinks telnet vim bc tree bash-completion bash-completion-extras screen curl yum-utils net-tools git openssl pstree mongodb-org-shell lsof "
